34d94ae33f8056c9abef3abd7329a4b849f53f8b
Pavlo Grytsenko <pavlo@usf.edu>
2020-01-28 17:20:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PG_Conditional_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
